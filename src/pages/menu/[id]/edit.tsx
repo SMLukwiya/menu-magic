91aa36871ce6282c7b91349a25a60267d7684ef0
SMLukwiya <sundaymorganl@gmail.com>
2023-05-13 08:22:47 +0300

    remove unneccesary files

--- src/pages/menu/[id]/edit.tsx
+++ src/pages/menu/[id]/edit.tsx
@@ -1,12 +1,11 @@
 import { type NextPage } from "next";
-import { EditMenuForm } from "@/components/menu/pages/edit.page";
 
 type Props = {
   id: string;
 };
 
 const EditMenu: NextPage<Props> = ({ id }: Props) => {
-  return <EditMenuForm id={id} />;
+  return <></>;
 };
 
 export function getServerSideProps(context: { params: Props }) {
